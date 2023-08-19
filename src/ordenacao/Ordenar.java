3533c096cd14f20db3baa042c098f6b59448c8e5
Eduardo Estigarribia <eduardo.estigarribia@gmail.com>
2023-08-19 11:02:21 -0300

    Retira maven

--- src/ordenacao/Ordenar.java
+++ src/ordenacao/Ordenar.java
@@ -43,4 +43,44 @@ public class Ordenar {
         vetor[j]=pivo;
         return j;
     }
+
+    public static void quick(int[] vetor){
+        quick(vetor,0, vetor.length-1);
+
+    }
+    private static void quick(int[] vetor, int esquerda, int direita){
+
+        if(direita>esquerda){
+            int pivo = divide(vetor, esquerda, direita);
+            quick(vetor, esquerda, pivo-1);
+            quick(vetor, pivo + 1, direita);
+        }
+
+
+    }
+
+    private static int divide(int[] vetor, int esquerda, int direita) {
+        int pivo=vetor[esquerda];
+        int i=esquerda+1;
+        int j=direita;
+        while (i<=j){
+            while (i<=j && pivo>=vetor[i]){
+                i++;
+            }
+            while (i<=j && pivo<vetor[j]){
+                j--;
+            }
+            if(i<j){
+                int aux = vetor[i];
+                vetor[i] = vetor[j];
+                vetor[j] = aux;
+
+            }
+        }
+        vetor[esquerda]=vetor[j];
+        vetor[j]=pivo;
+        return j;
+    }
+
+
 }
